445a28081aca008d5c540ab9911d00a191ae20e9
OceanBiscuit <1245298808@qq.com>
2019-12-31 17:20:18 +0800

    map类型ConcurrentHashMap的注释

--- common/src/main/java/com/management/platform/base/consts/ConstantsContext.java
+++ common/src/main/java/com/management/platform/base/consts/ConstantsContext.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.File;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
 import static com.management.platform.base.consts.ConfigConstant.SYSTEM_CONSTANT_PREFIX;
 import static cn.stylefeng.roses.core.util.ToolUtil.getTempPath;
 
@@ -17,6 +16,15 @@ import static cn.stylefeng.roses.core.util.ToolUtil.getTempPath;
  * @author fengshuonan
  * @Date 2019-06-20 13:37
  */
+/*
+ConcurrentHashMap底层采用分段的数组+链表实现，线程安全
+通过把整个Map分为N个Segment，可以提供相同的线程安全，但是效率提升N倍，默认提升16倍。(读操作不加锁，由于HashEntry的value变量是 volatile的，也能保证读取到最新的值。)
+Hashtable的synchronized是针对整张Hash表的，即每次锁住整张表让线程独占，ConcurrentHashMap允许多个修改操作并发进行，其关键在于使用了锁分离技术
+有些方法需要跨段，比如size()和containsValue()，它们可能需要锁定整个表而而不仅仅是某个段，这需要按顺序锁定所有段，操作完毕后，又按顺序释放所有段的锁
+扩容：段内扩容（段内元素超过该段对应Entry数组长度的75%触发扩容，不会对整个Map进行扩容），插入前检测需不需要扩容，有效避免无效扩容
+
+主要是快
+*/
 @Slf4j
 public class ConstantsContext {
 
@@ -25,6 +33,7 @@ public class ConstantsContext {
     /**
      * 所有的常量，可以增删改查
      */
+
     private static Map<String, Object> CONSTNTS_HOLDER = new ConcurrentHashMap<>();
 
     /**
