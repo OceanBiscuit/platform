6e4a35668daf80405fd4aa9e591a6916b481184c
OceanBiscuit <1245298808@qq.com>
2019-12-31 16:59:56 +0800

    添加jwt简单注解

--- common/src/main/java/com/management/platform/auth/jwt/payload/JwtPayLoad.java
+++ common/src/main/java/com/management/platform/auth/jwt/payload/JwtPayLoad.java
@@ -6,7 +6,24 @@ import java.util.Map;
 
 /**
  * jwt的第二部分
+ * 第一部分为Header:典型由两部分组成，token类型（JWT）和算法名称（比如：HMAC SHA256或者RSA等等）。
+ *例如：
+ * {
+ * "alg":"HS256 ,
+ * "type":"JWT"
+ *}BASE64对这个header的json进行编码得到第一部分
  *
+ * 第二部分就是payload
+ * 例如：
+ * {
+ *     "sub":"1231231"
+ *     "name":"john doe",
+ *     "admin":true
+ * }BASE64对这个payload的json进行编码得到第二部分
+ *
+ * 第三部分：
+ * HMACSHA256(base64UrlEncode(header) + "." + base64UrlEncode(payload), secret)
+ * 签名是用于验证消息在传递过程中有没有被更改，并且，对于使用私钥签名的token，它还可以验证JWT的发送方是否为它所称的发送方。
  * @author fengshuonan
  * @Date 2019/7/20 20:45
  */
